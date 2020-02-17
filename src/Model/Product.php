61c4072a2d01ebe596904db3dd8bd28b45d60c97
Verquerre Clara <verquerre@b11p3.fil.univ-lille1.fr>
2020-02-17 14:08:47 +0100

    :sparkles: add getters and setters for product model

--- src/Model/Product.php
+++ src/Model/Product.php
@@ -1,5 +1,6 @@
 <?php
 
+// Model class of a product.
 class Product
 {
     var $id;
@@ -7,4 +8,45 @@ class Product
     var $brand;
     var $price;
     var $details;
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($newId) {
+        $this->id = $newId;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($newName) {
+        $this->name = $newName;
+    }
+
+    public function getBrand() {
+        return $this->brand;
+    }
+
+    public function setBrand($newBrand) {
+        $this->brand = $newBrand;
+    }
+
+    public function getPrice() {
+        return $this->price;
+    }
+
+    public function setPrice($newPrice) {
+        $this->price = $newPrice;
+    }
+
+    public function getDetails() {
+        return $this->details;
+    }
+
+    public function setDetails($newDetails) {
+        $this->details = $newDetails;
+    }
 }
+
